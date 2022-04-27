9b156c6142f24c82c3c665333276226c93ce4b88
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2022-04-27 09:16:57 -0700

    scss trying again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
+const path = require('path')
+module.exports = {
+    trailingSlash: true,
+	webpackDevMiddleware: config => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300
+		}
+		return config
+	},
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
     images: {
         // loader: "cloudinary",
         // path: "https://res.cloudinary.com/the-color-mill/image/upload/",
@@ -13,4 +24,4 @@ module.exports = withSass({
         API_KEY: process.env.API_KEY,
     },
     reactStrictMode: true,
-});
+};
